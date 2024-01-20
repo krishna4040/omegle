69ab1829df7cfc0928fc161d69c7caedcf5ed137
krishna jain <115697713+krishna4040@users.noreply.github.com>
2024-01-20 19:53:51 +0530

    frontend basic setup

---
+++ src/components/Landing.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Landing = () => {
+
+    const [name, setName] = useState('');
+
+    return (
+        <div>
+            <input type="text" value={name} placeholder='Enter Name' onChange={(e) => setName(e.target.value)} />
+            <Link to={`/room?name=${name}`}>Join</Link>
+        </div>
+    )
+}
+
+export default Landing
---
+++ src/components/Room.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+const Room = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const name = searchParams.get('name');
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div>Room</div>
+    )
+}
+
+export default Room
